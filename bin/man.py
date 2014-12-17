dd08c50037688d6a1ecc9a2ff021c514fc2a00ed
Chris Houser <chris@housercompany.com>
2014-12-16 20:18:52 -0800

    Fixed man.py

--- bin/man.py
+++ bin/man.py
@@ -1,7 +1,9 @@
 '''
-Lists docstrings for file
+Display manual page (docstring) for scripts in /bin
 
-usage: man [file]
+usage: 
+    man - Lists all python scripts in bin.
+    man [script] - shows the docstring if availiable.
 '''
 import sys
 import os
@@ -14,13 +16,15 @@ for mod in modules:
     module_dict[re.sub(r'\.\w*','',mod)]=mod
     
 if len(sys.argv) == 1:
-    for mod in module_dict.keys():
+    key_list = module_dict.keys()
+    key_list.sort()
+    for mod in key_list:
         print mod
         #print mod.replace('.*','',mod)
 if len(sys.argv) >1:
     search_str = sys.argv[1]
     output = ''
-    expr = re.compile(r"'''(.*)'''",re.S)
+    expr = re.compile(r".*?[',\"]{3}(.*?)[',\"]{3}",re.S)
     with open(path+'/'+module_dict[search_str],'r') as f:
         out = f.read()
         try:
