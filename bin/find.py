39e4f95179c85b4aa9a0cbd0310b1b47368f00bf
Yang Wang <ywangd@gmail.com>
2015-01-14 00:20:09 +1100

    Find, xargs enhanced. ls also improved.

--- bin/find.py
+++ bin/find.py
@@ -4,28 +4,117 @@
 import os
 import sys
 import argparse
+import time
 import fnmatch
+from functools import partial
+
+
+class FilePredicate(object):
+
+    def __init__(self):
+        self.funclist = []
+
+    def add_filter(self, func):
+        self.funclist.append(func)
+
+    def run(self, paths):
+        names = []
+        for pth in paths:
+            for root, dirs, files in os.walk(pth):
+                for func in self.funclist:
+                    root, dirs, files = func(root, dirs, files)
+                    if root is None:
+                        break
+                if root is not None:
+                    names.extend(os.path.join(root, f) for f in files)
+                    names.extend(os.path.join(root, d + '/') for d in dirs)
+
+        return names
+
+
+def filter_type(ftype, root, dirs, files):
+    if ftype == 'a':
+        return root, dirs, files
+
+    elif ftype == 'f':
+        if not dirs and not files:
+            return None, None, None
+        else:
+            return root, dirs, files
+
+    elif ftype == 'd':
+        return root, dirs, []
+
+def filter_name(pattern, root, dirs, files):
+    files = fnmatch.filter(files, pattern)
+    dirs = fnmatch.filter(dirs, pattern)
+    if not files and not dirs and not fnmatch.fnmatch(root, pattern):
+        return None, None, None
+    return root, dirs, files
+
+def filter_mtime(oldest_time, newest_time, root, dirs, files):
+    fnames = []
+    for f in files:
+        st_mtime = os.stat(os.path.join(root, f)).st_mtime
+        if newest_time > st_mtime > oldest_time:
+            fnames.append(f)
+
+    dnames = []
+    for d in dirs:
+        st_mtime = os.stat(os.path.join(root, d)).st_mtime
+        if newest_time > st_mtime > oldest_time:
+            dnames.append(d)
+
+    if not fnames and not dnames:
+        st_mtime = os.stat(root).st_mtime
+        if not (newest_time > st_mtime > oldest_time):
+            return None, None, None
+
+    return root, dnames, fnames
 
 
 def main(args):
     ap = argparse.ArgumentParser()
     ap.add_argument('paths', nargs='+', help='specify a file hierarchy for find to traverse')
     ap.add_argument('-n', '-name', '--name', dest='pattern',
-                    nargs='?', help='pattern to match file names')
+                    nargs='?',
+                    default='*',
+                    help='pattern to match file names')
     ap.add_argument('-t', '-type', '--type',
-                    nargs='?', help='specify the file type to match.\nf - regular file\nd - directory')
+                    nargs='?',
+                    default='f',
+                    choices=('a', 'f', 'd'),
+                    help='specify the file type to match')
+    ap.add_argument('-d', '-mtime', '--mtime',
+                    metavar='n',
+                    nargs='?',
+                    help='specify modification time range')
+    ap.add_argument('-maxdepth', '--maxdepth',
+                    metavar='n',
+                    nargs='?',
+                    help='descend at most n directory levels below command line arguments')
     ns = ap.parse_args(args)
 
-    pattern = '*' if ns.pattern is None else ns.pattern
-    ftype = ns.type
+    file_predicate = FilePredicate()
+    file_predicate.add_filter(partial(filter_type, ns.type))
+    file_predicate.add_filter(partial(filter_name, ns.pattern))
 
-    names = []
-    for pth in ns.paths:
-        for root, dirs, files in os.walk(pth):
-            if ftype is None or ftype == 'f':
-                names.extend(os.path.join(root, name) for name in fnmatch.filter(files, pattern))
-            if ftype is None or ftype == 'd':
-                names.extend(os.path.join(root, name + '/') for name in fnmatch.filter(dirs, pattern))
+    if ns.mtime:
+        oldest_time = 0
+        tnow = newest_time = time.time()
+        if ns.mtime.startswith('-'):
+            ndays = int(ns.mtime[1:])
+            oldest_time = tnow - ndays * 86400.0
+        elif ns.mtime.startswith('+'):
+            ndays = int(ns.mtime[1:])
+            newest_time = tnow - (ndays + 1) * 86400.0
+        else:
+            ndays = int(ns.mtime)
+            oldest_time = tnow - (ndays + 1) * 86400.0
+            newest_time = tnow - ndays * 86400.0
+        file_predicate.add_filter(partial(filter_mtime, oldest_time, newest_time))
+
+    names = file_predicate.run(ns.paths)
 
     print '\n'.join(names)
 
--- bin/xargs.py
+++ bin/xargs.py
@@ -9,17 +9,50 @@ _stash = globals()['_stash']
 
 def main(args):
     ap = argparse.ArgumentParser()
-    ap.add_argument('args_to_xargs', nargs='*', help='arguments to xargs')
+    ap.add_argument('-n',
+                    nargs='?',
+                    metavar='number',
+                    type=int,
+                    help='maximum number of arguments taken from standard input for each invocation of utility')
+
+    ap.add_argument('-I',
+                    dest='replstr',
+                    nargs='?',
+                    help='replacement string')
+
+    ap.add_argument('utility',
+                    nargs='?',
+                    default='echo',
+                    help='utility to invoke')
+
+    ap.add_argument('args_to_pass',
+                    metavar='arguments',
+                    nargs=argparse.REMAINDER,
+                    help='arguments to the utility')
+
     ns = ap.parse_args(args)
-    args_to_xargs = ns.args_to_xargs
 
-    if args_to_xargs:
-        cmd = args_to_xargs.pop(0)
-    else:
-        cmd = 'echo'
+    lines = [line.strip() for line in sys.stdin.readlines()]
+    n = ns.n if ns.n else len(lines)
+    if ns.replstr:
+        n = 1
 
-    for line in sys.stdin.readlines():
-        _stash('%s %s' % (cmd, ' '.join(args_to_xargs + [line.strip()])))
+    while lines:
+        rest = ' '.join(lines[:n])
+        lines = lines[n:]
+        args_to_pass = ' '.join(ns.args_to_pass)
+
+        if rest.strip():
+
+            if ns.replstr:
+                args_to_pass = args_to_pass.replace(ns.replstr, rest)
+                rest = ''
+
+            cmdline = '%s %s %s' % (ns.utility,
+                                    args_to_pass,
+                                    rest)
+
+            _stash(cmdline)
 
 
 if __name__ == "__main__":
