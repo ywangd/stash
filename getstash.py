59e69bb6f7389426b6054356a14fdcd746087eb9
cclauss <cclauss@bluewin.ch>
2015-02-01 22:39:25 +0100

    Minor edits

--- getstash.py
+++ getstash.py
@@ -5,7 +5,7 @@ import random
 
 URLBASE = 'https://raw.githubusercontent.com/ywangd/stash/master'
 
-FNAMES = ['stash.py', 'bin/selfupdate.sh', 'bin/echo.py', 'bin/wget.py', 'bin/unzip.py', 'bin/rm.py', 'bin/alias.py']
+FNAMES = 'stash.py bin/selfupdate.sh bin/echo.py bin/wget.py bin/unzip.py bin/rm.py bin/alias.py'.split()
 
 HOME2 = os.path.expanduser('~/Documents')
 
@@ -16,17 +16,16 @@ if not os.path.exists(STASH_ROOT):
     os.mkdir(STASH_ROOT)
 
 os.chdir(STASH_ROOT)
-if not os.path.exists('lib'):
-    os.mkdir('lib')
-if not os.path.exists('bin'):
-    os.mkdir('bin')
+for x in ('lib', 'bin'):
+    if not os.path.exists(x):
+        os.mkdir(x)
 
 print 'Downloading files ...'
 try:
     for fname in FNAMES:
         # Random number to force refresh
         url = "%s/%s?q=%d" % (URLBASE, fname, random.randint(1, 999999))
-        print url
+        print(url)
         req = urllib2.Request(url)
         req.add_header('Cache-Control', 'no-cache')
         contents = urllib2.urlopen(req).read()
