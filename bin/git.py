786a143d186b7b9959db32dbe4c169f74a135c09
jsbain <jsbain@yahoo.com>
2015-02-16 10:09:32 +0000

    fix most porcelain functions, which dont seem to want to accept a repo object anymore.  added refresh_editor, to reload current editor file when checking out.  added soft mode for reset, to just update head without chaning anything else.  updated dulwich install code to check for version, and hopefully still work when dulwich was installed already ina nother location

--- bin/git.py
+++ bin/git.py
@@ -12,6 +12,8 @@ Commands:
     log: git log - Options:\n\t[-l|--length  numner_of _results]\n\t[-f|--format format string can use {message}{author}{author_email}{committer}{committer_email}{merge}{commit}]\n\t[-o|--output]  file_name
     push: git push [http(s)://<remote repo>] [-u username[:password]] - push changes back to remote
     pull: git pull [http(s)://<remote repo> or remote] - pull changes from a remote repository
+    merge: git merge <merge_commit> - merge another branch or commit and head into current working tree.   see git merge -h
+    fetch: git fetch [uri or remote] - fetch changes from remote
     checkout: git checkout <branch> - check out a particular branch in the Git tree
     branch: git branch - show branches
     remote: git remote [remotename remoteuri]- list or add remote repos 
@@ -27,46 +29,65 @@ import argparse
 import getpass
 import urlparse,urllib2,keychain
 import sys,os,posix
-
+import editor #for reloading current file
 # temporary -- install required modules
 
 GITTLE_URL='https://github.com/jsbain/gittle/archive/master.zip'
 FUNKY_URL='https://github.com/FriendCode/funky/archive/master.zip'
 DULWICH_URL='https://github.com/jsbain/dulwich/archive/master.zip'
+REQUIRED_DULWICH_VERSION = (0,9,9,'jsbain_fork')
+AUTODOWNLOAD_DEPENDENCIES = True 
 
-if True:
+if AUTODOWNLOAD_DEPENDENCIES:
     libpath=os.path.join(_stash.runtime.envars['STASH_ROOT'] ,'lib')
     if not libpath in sys.path:
         sys.path.insert(1,libpath)
     download_dulwich = False 
+    
+    #DULWICH
     try:  
         import dulwich
         from dulwich.client import default_user_agent_string
         from dulwich import porcelain
         from dulwich.index import index_entry_from_stat
-        if not hasattr(porcelain, 'fetch'):
+        if not dulwich.__version__ ==  REQUIRED_DULWICH_VERSION:
+            print 'Dulwich version was {}.  Required is {}.  will download'.format(dulwich.__version__,REQUIRED_DULWICH_VERSION)
             download_dulwich = True
     except ImportError as e:
+        print 'dulwich was not found.  Will attempt to download. '
         download_dulwich = True 
-    
-    if download_dulwich:
-        _stash('wget {} -o $TMPDIR/dulwich.zip'.format(DULWICH_URL))
-        _stash('unzip $TMPDIR/dulwich.zip -d $TMPDIR/dulwich')
-        _stash('rm -r $STASH_ROOT/lib/dulwich.old')
-        _stash('mv $STASH_ROOT/lib/dulwich $STASH_ROOT/lib/dulwich.old')
-        _stash('mv $TMPDIR/dulwich/dulwich $STASH_ROOT/lib/')
-        _stash('rm  $TMPDIR/dulwich.zip')
-        _stash('rm -r $TMPDIR/dulwich')
-        _stash('rm -r $STASH_ROOT/lib/dulwich.old')
-        try: 
-            reload(dulwich)
-            reload(dulwich.porcelain)
-        except NameError:
-            pass 
-        import dulwich
-        from dulwich import porcelain
-        from dulwich.client import default_user_agent_string
-                
+    try:
+        if download_dulwich:
+            _stash('wget {} -o $TMPDIR/dulwich.zip'.format(DULWICH_URL))
+            _stash('unzip $TMPDIR/dulwich.zip -d $TMPDIR/dulwich')
+            _stash('rm -r $STASH_ROOT/lib/dulwich.old')
+            _stash('mv $STASH_ROOT/lib/dulwich $STASH_ROOT/lib/dulwich.old')
+            _stash('mv $TMPDIR/dulwich/dulwich $STASH_ROOT/lib/')
+            _stash('rm  $TMPDIR/dulwich.zip')
+            _stash('rm -r $TMPDIR/dulwich')
+            _stash('rm -r $STASH_ROOT/lib/dulwich.old')
+            try: 
+                # dulwich might have already been in site-packages for instance.  
+                # So, some acrobatic might be needed to unload the module
+                if 'dulwich' in sys.modules:
+                    for m in [m for m in sys.modules if m.startswith('dulwich')]:
+                        del sys.modules[m]
+                import dulwich
+                reload(dulwich)
+            except NameError:
+                pass 
+            #try the imports again
+            import dulwich
+            from dulwich.client import default_user_agent_string
+            from dulwich import porcelain
+            from dulwich.index import index_entry_from_stat
+    except Exception:
+        print '''Still could not import dulwich.
+            Perhaps your network connection was unavailable.
+            You might also try deleting any existing dulwich versions in site-packages or elsewhere, then restarting pythonista.'''
+
+    #gittle, funky
+    # todo... check gittle version
     try:
         gittle_path=os.path.join(libpath,'gittle')
         funky_path=os.path.join(libpath,'funky')
@@ -87,8 +108,9 @@ if True:
         Gittle=gittle.Gittle
     ## end install modules
 else:
-    from dulwich import porcelain
+    import dulwich
     from dulwich.client import default_user_agent_string
+    from dulwich import porcelain
     from dulwich.index import index_entry_from_stat
     from gittle import Gittle
 
@@ -201,7 +223,7 @@ def git_init(args):
 def git_status(args):
     if len(args) == 0:
         repo = _get_repo()
-        status = porcelain.status(repo.repo)
+        status = porcelain.status(repo.repo.path)
         print 'STAGED'
         for k,v in status.staged.iteritems():
             if v:
@@ -234,7 +256,7 @@ def git_add(args):
         for file in args:
             if os.path.exists(file):
                 print 'Adding {0}'.format(file)
-                porcelain.add(repo.repo, [file])
+                porcelain.add(repo.repo.path, [file])
             else:
                 print '{} does not exist. skipping'.format(file)
 
@@ -250,7 +272,7 @@ def git_rm(args):
         for file in args:
             print 'Removing {0}'.format(file)
             #repo.rm(args)
-            porcelain.rm(repo.repo, args)
+            porcelain.rm(repo.repo.path, args)
 
     else:
         print command_help['rm']
@@ -267,18 +289,20 @@ def git_merge(args):
     launch_subcmd('git-merge.py',args)
 
 def git_reset(args):
+    import git.gitutils as gitutils
     ap=argparse.ArgumentParser('reset')
     ap.add_argument('commit',nargs='?',action='store',default='HEAD')
     ap.add_argument('paths',nargs='*')
     mode=ap.add_mutually_exclusive_group()
-    mode.add_argument('--hard',action='store_true',dest='hard')
-    mode.add_argument('--mixed',action='store_false',dest='hard')
+    mode.add_argument('--hard',action='store_true')
+    mode.add_argument('--mixed',action='store_true')
+    mode.add_argument('--soft',action='store_true')
+ 
     ap.add_argument('--merge',action='store_true')
     ns=ap.parse_args(args)
-    
-    hard=ns.hard
+
         
-    repo = _get_repo().repo
+    repo = _get_repo()
     
     if ns.merge:
         try:
@@ -291,25 +315,35 @@ def git_reset(args):
     commit= ns.commit
     # first arg was really a file
     paths=ns.paths or []
-    if not commit in _get_repo() and os.path.exists(commit):
+    if not commit in repo and os.path.exists(commit): #really specified a path
         paths=[commit]+paths
         commit = None
-    elif not commit in _get_repo() and not os.path.exists(commit):
+    elif not commit in repo and not commit in repo.branches and not commit in repo.remote_branches and not os.path.exists(commit):
         raise Exception('{} is not a valid commit or file'.format(commit))
     if not commit:
-        commit=repo.refs['HEAD']
+        commit='HEAD'
     
-    if hard:
+    if not paths:
+        #reset HEAD, if commit in branches
+        if commit in repo.branches or commit == 'HEAD':
+            print 'updating head'
+            repo.repo.refs.set_symbolic_ref('HEAD',repo._format_ref_branch(commit))
+        else:
+            print commit, 'is not a valid branchname.  head was not updated'
+    if ns.hard:
         _confirm_dangerous()
+ 
+    if ns.hard or ns.mixed:
     # first, unstage index
-    if paths:
-        unstage(commit,paths)
-    else:
-        print 'resetting index. please wait'
-        unstage_all(commit)
-        print 'complete'
+        if paths:
+            unstage(commit,paths)
+        else:
+            print 'resetting index. please wait'
+            unstage_all(commit)
+            print 'complete'
+ 
     # next, rebuild files
-    if hard:
+    if ns.hard:
         treeobj=repo[repo[commit].tree]
         
         for path in paths:
@@ -358,6 +392,7 @@ def git_commit(args):
     try:
     
         author = "{0} <{1}>".format(ns.name, ns.email)
+
         print repo.repo.do_commit(message=ns.message
                                   , author=author
                                   , committer=author 
@@ -504,11 +539,11 @@ def git_push(args):
 
         opener = auth_urllib2_opener(None, result.url, user, pw)
 
-        porcelain.push(repo.repo, result.url, branch_name, opener=opener)
+        porcelain.push(repo.repo.path, result.url, branch_name, opener=opener)
         keychain.set_password(keychainservice, user, pw)
 
     else:
-        porcelain.push(repo.repo, result.url, branch_name)
+        porcelain.push(repo.repo.path, result.url, branch_name)
     print 'success!'
 
 def git_modified(args):
@@ -538,7 +573,7 @@ def git_log(args):
 
     try:
         repo = _get_repo()
-        porcelain.log(repo.repo, max_entries=results.max_entries,format=results.format,outstream=results.output)
+        porcelain.log(repo.repo.path, max_entries=results.max_entries,format=results.format,outstream=results.output)
     except ValueError:
         print command_help['log']
 
@@ -569,7 +604,7 @@ def git_checkout(args):
                 repo.repo.refs.set_symbolic_ref('HEAD',branch_ref)
                 repo.checkout_all()
             repo.switch_branch('{0}'.format(args[0]))
-
+    
         #Temporary hack to get create branch into source
         #TODO: git functions should probably all user parseargs, like git push
         if len(args) == 2:
@@ -579,9 +614,24 @@ def git_checkout(args):
                 repo.create_branch(repo.active_branch, args[1], tracking=None)
                 #Recursive call to checkout the branch we just created
                 git_checkout([args[1]])
+        else:
+            refresh_editor()
     else:
         print command_help['checkout']
         
+def refresh_editor():
+    #reload current file in editor
+    # TODO: only reload if the file was recently updated...
+    try:
+        sel=editor.get_selection()
+        editor.open_file(editor.get_path())
+        import time
+        time.sleep(0.5) #let the file load
+        editor.replace_text(sel[0],sel[0],'') #force scroll
+        editor.set_selection(sel[0],sel[1])
+    except:
+        print 'Could not refresh editor.  continuing anyway'
+    
 def git_help(args):
     print 'help:'
     for key, value in command_help.items():
