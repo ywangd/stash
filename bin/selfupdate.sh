8315c9b0693cf2586dc3a442cb79c50528f3d402
Yang Wang <ywangd@gmail.com>
2015-03-06 16:03:00 +1100

    Documents updated for 0.4.1 release.
    Removed pcsm.py as it is not ready for new scope of the design.

--- bin/selfupdate.sh
+++ bin/selfupdate.sh
@@ -7,4 +7,5 @@ rm -f $STASH_ROOT/dummyui.py
 rm -f $STASH_ROOT/dummyconsole.py
 rm -f $STASH_ROOT/testing.py
 rm -rf $STASH_ROOT/tests
+rm -f $STASH_ROOT/bin/pcsm.py $STASH_ROOT/bin/bh.py $STASH_ROOT/bin/pythonista.py $STASH_ROOT/bin/cls.py
 echo Done
