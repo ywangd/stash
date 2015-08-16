cc8ed7ef30d9d1a60d2cd61f47898d63fa6bcebb
Georg Viehoever <georg.viehoever>
2015-08-16 19:41:56 +0200

    improved handling of sys.path
    Better manipulation of sys.path, in particular avoiding constant growth

--- bin/wol.py
+++ bin/wol.py
@@ -37,40 +37,40 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 import sys
+import sysconfig
 import os
 import os.path
 
-# manipulate sys.path to include stash lib directory (where wakeonlan is found)
-# and have stdlib before .
-pythonLibPath=os.path.join(os.path.split(sys.executable)[0],'lib')
-sys.path.insert(0,pythonLibPath)
-
+# mechanism to avoid constant extension of sys.path
+savedSysPath=list(sys.path)
 try:
-    # while running in stash environment
-    stashLibPath=os.path.join(_stash.runtime.envars['STASH_ROOT'] ,'lib')
-except NameError:
-    # while running on PC
-    stashLibPath=os.path.join(os.environ['STASH_ROOT'],'lib')
-if not stashLibPath in sys.path:
+    # manipulate sys.path to include stash lib directory (where wakeonlan is found)
+    # and have stdlib before "."
+    pythonLibPath=sysconfig.get_path('stdlib')
+    sys.path.insert(0,pythonLibPath)
+
+    stashLibPath=os.path.join(os.environ['STASH_ROOT'] ,'lib')
     sys.path.insert(0,stashLibPath)
 
-import argparse
-from wakeonlan import wol
+    import argparse
+    from wakeonlan import wol
 
 
-parser = argparse.ArgumentParser(
-    description="""Wake one or more computers using the wake on lan protocol.
-Note that this requires suitable configuration of the target system, and only
-works within a network segment (i.e. not across routers or VPN).""")
-parser.add_argument(
-    'macs', metavar='mac addresses', nargs='+',
-    help='The mac addresses or of the computers you are trying to wake, for instance 40:16:7e:ae:af:43')
-parser.add_argument(
-    '-i', metavar='ip', default=wol.BROADCAST_IP,
-    help='The ip address of the host to send the magic packet to. (default {})'
-    .format(wol.BROADCAST_IP))
-parser.add_argument(
-    '-p', metavar='port', default=wol.DEFAULT_PORT,
-    help='The port of the host to send the magic packet to (default 9)')
-args = parser.parse_args()
-wol.send_magic_packet(*args.macs, ip_address=args.i, port=args.p)
+    parser = argparse.ArgumentParser(
+        description="""Wake one or more computers using the wake on lan protocol.
+    Note that this requires suitable configuration of the target system, and only
+    works within a network segment (i.e. not across routers or VPN).""")
+    parser.add_argument(
+        'macs', metavar='mac addresses', nargs='+',
+        help='The mac addresses or of the computers you are trying to wake, for instance 40:16:7e:ae:af:43')
+    parser.add_argument(
+        '-i', metavar='ip', default=wol.BROADCAST_IP,
+        help='The ip address of the host to send the magic packet to. (default {})'
+        .format(wol.BROADCAST_IP))
+    parser.add_argument(
+        '-p', metavar='port', default=wol.DEFAULT_PORT,
+        help='The port of the host to send the magic packet to (default 9)')
+    args = parser.parse_args()
+    wol.send_magic_packet(*args.macs, ip_address=args.i, port=args.p)
+finally:
+    sys.path=savedSysPath
