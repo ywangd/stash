7209c47ddd255c8d5a751d69bb9e987c0f1c0ff3
Yang Wang <ywangd@gmail.com>
2016-01-13 21:44:29 +1100

    Added argparse to selfupdate to prepare future functions.
    Bump version to 0.5.1

--- bin/selfupdate.py
+++ bin/selfupdate.py
@@ -8,31 +8,42 @@ Usage: selfupdate.py [branch]
 """
 import os
 import sys
+from argparse import ArgumentParser
 
 _stash = globals()['_stash']
 
-try:
-    SELFUPDATE_BRANCH = sys.argv[1]
-except IndexError:
-    SELFUPDATE_BRANCH = os.environ.get('SELFUPDATE_BRANCH', 'master')
+def main(args):
 
-print _stash.text_style('StaSh is trying to selfupdate ...',
-                        {'color': 'yellow', 'traits': ['bold']})
-print u'%s: %s' % (_stash.text_bold('branch'), SELFUPDATE_BRANCH)
+    ap = ArgumentParser()
+    ap.add_argument('branch', nargs='?', help='The branch to update to')
+    ns = ap.parse_args(args)
 
-url = 'https://raw.githubusercontent.com/ywangd/stash/%s/getstash.py' % SELFUPDATE_BRANCH
-print u'%s: %s' % (_stash.text_bold('url'),
-                   _stash.text_style(url, {'color': 'blue', 'traits': ['underline']}))
+    if ns.branch is not None:
+        SELFUPDATE_BRANCH = ns.branch
+    else:
+        SELFUPDATE_BRANCH = os.environ.get('SELFUPDATE_BRANCH', 'master')
 
-import urllib2
-from random import randint
+    print _stash.text_style('StaSh is trying to selfupdate ...',
+                            {'color': 'yellow', 'traits': ['bold']})
+    print u'%s: %s' % (_stash.text_bold('branch'), SELFUPDATE_BRANCH)
 
-try:
-    exec urllib2.urlopen(
-        'https://raw.githubusercontent.com/ywangd/stash/%s/getstash.py?q=%s' % (SELFUPDATE_BRANCH,
-                                                                                randint(1, 999999))
-    ).read() in {'_IS_UPDATE': True, '_br': SELFUPDATE_BRANCH}
-    print _stash.text_color('Update completed.', 'green')
-    print _stash.text_color('Please restart StaSh to ensure changes becoming effective.', 'green')
-except SystemExit:
-    print _stash.text_color('Failed to update. Please try again.', 'red')
+    url = 'https://raw.githubusercontent.com/ywangd/stash/%s/getstash.py' % SELFUPDATE_BRANCH
+    print u'%s: %s' % (_stash.text_bold('url'),
+                       _stash.text_style(url, {'color': 'blue', 'traits': ['underline']}))
+
+    import urllib2
+    from random import randint
+
+    try:
+        exec urllib2.urlopen(
+            'https://raw.githubusercontent.com/ywangd/stash/%s/getstash.py?q=%s' % (SELFUPDATE_BRANCH,
+                                                                                    randint(1, 999999))
+        ).read() in {'_IS_UPDATE': True, '_br': SELFUPDATE_BRANCH}
+        print _stash.text_color('Update completed.', 'green')
+        print _stash.text_color('Please restart Pythonista to ensure changes becoming effective.', 'green')
+    except SystemExit:
+        print _stash.text_color('Failed to update. Please try again.', 'red')
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
--- stash.py
+++ stash.py
@@ -5,7 +5,7 @@ StaSh - Pythonista Shell
 https://github.com/ywangd/stash
 """
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 import os
 import sys
