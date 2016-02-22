970b4e74b2e77031f35f18efd36393d89332eebe
Yang Wang <ywangd@gmail.com>
2016-02-23 08:22:41 +1100

    Fixed tests for Python 2.

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,7 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import unicode_literals
 import os
+import sys
 import argparse
 import unittest
 
@@ -12,13 +13,11 @@ ap.add_argument('-v', '--verbose', action='store_true', help='be more chatty')
 ap.add_argument('-p', '--pattern', default='test_*.py', help='the pattern to search test files')
 ns = ap.parse_args()
 
-top_level_dir = os.path.abspath(os.path.dirname(__file__))
-print(top_level_dir)
+stash_dir = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(stash_dir))
 
-
-testsuite = unittest.defaultTestLoader.discover('.',
-                                                pattern=ns.pattern,
-                                                top_level_dir=top_level_dir)
+testsuite = unittest.defaultTestLoader.discover('system',
+                                                pattern=ns.pattern)
 runner = unittest.TextTestRunner(verbosity=2 if ns.verbose else 1)
 
 runner.run(testsuite)
