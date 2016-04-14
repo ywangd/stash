7cf00f9ae015f3e0fde62e67c90124f44ab39a9b
Yang Wang <ywangd@gmail.com>
2016-04-14 11:13:09 +1000

    Removed future imports from selfupdate so it can work with other py2 branches.

--- bin/selfupdate.py
+++ bin/selfupdate.py
@@ -9,9 +9,6 @@ Usage: selfupdate.py [-n] [-f] [branch]
        -n, --check    check for update only
        -f, --force    update without checking for new version
 """
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
 from __future__ import unicode_literals
 import os
 import sys
