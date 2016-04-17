5dcaa909fb916350ce03be224c48208ed7ff5616
bennr01 <bennr01@users.noreply.github.com>
2016-04-17 20:09:31 +0200

    added interactive console
    -added a interactive console (started when not called with "-c","-m" or file argument)
    -changed the way the command-line arguments are handled.
    -changed the help-paramters to be more like CPython
    -fixed a bug allowing to pass both "-c" and "-d" (it now prints an error-message)
    -converted spaces to tabs (i edited this file in pythonista, it is annoying to see a mixture of tabs and spaces)

--- bin/python.py
+++ bin/python.py
@@ -1,39 +1,57 @@
 """
-Simulates a console call to python -m module|file [args]
+Simulates a console call to python [-m module][-c cmd] [file] [args]
 
 Used for running standard library python modules such as:
 SimpleHTTPServer, unittest and .py files.
 
 Can also be used to run a script in the background, such as a server, with the bash character & at the end.
 usage:
+    python
     python -m module_name [args]
     python python_file.py [args]
+    python -c cmd
 """
 
 import runpy
 import sys
 import argparse
+import code
+import __builtin__
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-m', '--module', action='store_true', default=False, help='run module')
-ap.add_argument('-c', '--cmd', action='store_true', default=False, help='program passed in as string')
-ap.add_argument('name', help='file or module name')
-ap.add_argument('args_to_pass', nargs=argparse.REMAINDER, help='args to pass')
+ap.add_argument('-m', '--module', action='store', default=None, help='run module')
+ap.add_argument('-c', '--cmd', action='store', default=None, help='program passed in as string (terminates option list)')
+ap.add_argument('file', action='store', default=None, help='program read from script file (terminates option list)', nargs='?')#,required=False)
+ap.add_argument('args_to_pass', nargs=argparse.REMAINDER, help='arguments passed to program in sys.argv[1:]')
 args = ap.parse_args()
 
 sys.argv = [sys.argv[0]] + args.args_to_pass
 
-if args.module:
-    try:
-        runpy.run_module(str(args.name), run_name='__main__')
-    except ImportError, e:
-        print 'ImportError: ' + str(e)
+if (args.module is not None) and (args.cmd is not None):
+	print 'Error: Please only pass either "-c" or "-m", but not both!'
+	sys.exit(1)
+elif args.module is not None:
+	try:
+		runpy.run_module(str(args.module), run_name='__main__')
+	except ImportError, e:
+		print 'ImportError: ' + str(e)
+		sys.exit(1)
+	sys.exit(0)
 
-elif args.cmd:
-    exec args.name
+elif args.cmd is not None:
+	exec args.cmd
+	sys.exit(0)
 
+elif args.file is not None:
+	try:
+		runpy.run_path(str(args.name), run_name='__main__')
+	except Exception, e:
+		print 'Error: ' + str(e)
 else:
-    try:
-        runpy.run_path(str(args.name), run_name='__main__')
-    except Exception, e:
-        print 'Error: ' + str(e)
+	locals={
+	'__name__':'__main__',
+	'__doc__':None,
+	'__package__':None,
+	'__builtins__':__builtin__,#yes, __builtins__
+	}
+	code.interact(local=locals)
