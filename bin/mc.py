f1164c935a2cba8cd1ff4e49524366ed69f298c4
bennr01 <bennr01@users.noreply.github.com>
2016-04-20 19:00:54 +0200

    mc.py: added FTP support and help
    edited mc.py:
       -now supports FTP
       -added help-texts

--- bin/mc.py
+++ bin/mc.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 """easily work on two filesystems (e.g. local and FTP)"""
 #the name refers to midnight-commander, but this will probald never be a true counterpart
-import os,shutil,cmd,sys
+import os,shutil,cmd,sys,ftplib,tempfile
 
 try:
-	stash=globals()["_stash"]
+	_stash=globals()["_stash"]
 except:
-	stash=None
+	_stash=None
 
 #======================
 #Errors
@@ -25,11 +25,11 @@ class BaseFSI(object):
 	"""
 Baseclass for all FSIs.
 Other FSIs should subclass this.
-This currently only serves as a documentation, but this mad change.
+This class currently only serves as a documentation, but this may change.
 """
-	def __init__(self):
+	def __init__(self,master):
 		"""called on __init__"""
-		pass
+		self.master=master
 	def connect(self,command):
 		"""
 called to 'connect' to a filesystem. 
@@ -54,7 +54,7 @@ This should return a list of strings.
 	def cd(self,name):
 		"""this should change the cwd to name."""
 		raise OperationFailure("NotImplemented")
-	def getpath(self):
+	def get_path(self):
 		"""this should return the current path as a string."""
 		return "/"
 	def remove(self,name):
@@ -74,7 +74,8 @@ This should return a list of strings.
 
 class LocalFSI(BaseFSI):
 	"""A FSI for the local filesystem."""
-	def __init__(self):
+	def __init__(self,master):
+		self.master=master
 		self.path=os.getcwd()
 	def connect(self,args):
 		return True#no setup required; connect is allways successful
@@ -137,14 +138,178 @@ class LocalFSI(BaseFSI):
 	def close(self):
 		pass
 
-#========================
+class FTPFSI(BaseFSI):
+	"""a FSI for FTP-server.
+Unfortunally, FTP was designed as a human-readable protocol.
+Due to this, the protocol is not completly unified.
+This means, that this FSI lad not work on all FTP-servers."""
+	def __init__(self,master):
+		self.master=master
+		self.path="/"
+		self.ftp=None
+		self.host=None
+	def connect(self,cmd):
+		if self.ftp is not None:
+			return "Interface already connected"
+		args=cmd.split()
+		if len(args)<1 or len(args)>5:
+			return "Invalid argument count"
+		user,pswd=None,None
+		debug=0
+		#TODO: make the following code less ugly
+		if len(args)==1:
+			host=args[0]
+			port=21
+			secure=False
+		elif len(args)==2:
+			host,port=args
+			secure=False
+		elif len(args)==5 or len(args)==4:
+			user=args[2]
+			pswd=args[3]
+			secure=False
+			host=args[0]
+			port=args[1]
+		if len(args) not in (3,5):
+			#this prevents the elifs from beeing execeuted
+			pass
+		elif args[-1]=="-s":
+			host,port=args[:2]
+			secure=True
+		elif args[-1]=="-n":
+			host,port=args[:2]
+			secure=False
+		elif args[-1]=="-d":
+			host,port=args[:2]
+			secure=True
+			debug=2
+		else:
+			return "Unknown argument(s)"
+		self.host=host
+		self.port=port
+		self.user=user
+		try:
+			port=int(port)
+		except:
+			return "Invalid port-argument"
+		if secure:
+			self.ftp=ftplib.FTP_TLS()
+		else:
+			self.ftp=ftplib.FTP()
+		self.ftp.set_debuglevel(debug)
+		try:
+			self.ftp.connect(host,port)
+		except Exception as e:
+			self.close()
+			if isinstance(e,EOFError):
+				return "EOF"
+			return e.message
+		else:
+			if secure:
+				self.master.stdout.write("Done.\nSecuring Connection... ")
+				try:
+					self.ftp.prot_p()
+				except Exception as e:
+					self.close()
+					return e.message
+				self.master.stdo
+			self.master.stdout.write("Done.\nLogging in... ")
+			try:
+				self.ftp.login(user,pswd)
+			except Exception as e:
+				self.close()
+				return e.message
+			else:
+				self.path=self.ftp.pwd()
+				return True
+	def close(self):
+		if self.ftp is not None:
+			try:
+				self.ftp.quit()
+			except:
+				try:
+					self.ftp.close()
+				except:
+					pass
+	def repr(self):
+		return "FTP-Session for {u} on {h}:{p}".format(u=self.user,h=self.host,p=self.port)
+	def cd(self,name):
+		ap=os.path.join(self.path,name)
+		try:
+			self.ftp.cwd(ap)
+		except Exception as e:
+			raise OperationFailure(str(e))
+		else:
+			self.path=ap
+	def mkdir(self,name):
+		ap=os.path.join(self.path,name)
+		try:
+			self.ftp.mkd(ap)
+		except Exception as e:
+			raise OperationFailure(str(e))
+	def listdir(self):
+		try:
+			return self.ftp.nlst(self.path)
+		except Exception as e:
+			raise OperationFailure(str(e))
+	def remove(self,name):
+		ap=os.path.join(self.path,name)
+		#we dont know wether target is a server or a file, so try both
+		try:
+			self.ftp.delete(ap)
+		except Exception as e:
+			try:
+				self.ftp.rmd(ap)
+			except Exception as e2:
+				self.master.stdout.write("Error trying to delete file: {e}!\n".format(e=e.message))
+				self.master.stdout.write("Error trying to delete dir (after file-deletion failed)!\n")
+				raise OperationFailure(e2.message)
+	def open(self,name,mode="rb"):
+		ap=os.path.join(self.path,name)
+		if mode in ("r","rb"):
+			try:
+				tf=tempfile.TemporaryFile()
+				self.ftp.retrbinary("RETR "+ap,tf.write,4096)
+				tf.seek(0)
+			except Exception as e:
+				raise OperationFailure(e.message)
+			return tf
+		elif "w" in mode:
+			return FTP_Upload(self.ftp,ap,mode)
+	def get_path(self):
+		return self.ftp.pwd()
+
+#=============================
+#utility classes and functions
+
+class FTP_Upload(object):
+	"""utility class used for FTP-uploads.
+this class creates a tempfile, which is uploded onto the server when closed."""
+	def __init__(self,ftp,path,mode):
+		self.ftp=ftp
+		self.path=path
+		self.mode=mode
+		self.tf=tempfile.TemporaryFile()
+	def write(self,data):
+		self.tf.write(data)
+	def close(self):
+		self.tf.seek(0)
+		try:
+			self.ftp.storbinary("STOR "+self.path,self.tf,4096)
+		except Exception as e:
+			raise OperationFailure(e.message)
+		finally:
+			self.tf.close()
+
+#=============================
 #Register FSIs here
 
 INTERFACES={
 "local":LocalFSI,
+"ftp":FTPFSI,
 }
 
-#=========================
+#=============================
 #User-Interface
 
 class McCmd(cmd.Cmd):
@@ -196,7 +361,7 @@ class McCmd(cmd.Cmd):
 			return
 		self.stdout.write("Creating Interface... ")
 		fsic=INTERFACES[name]
-		fsi=fsic()
+		fsi=fsic(self)
 		self.stdout.write("Done.\nConnecting... ")
 		try:
 			state=fsi.connect(args)
@@ -229,8 +394,8 @@ class McCmd(cmd.Cmd):
 		self.stdout.write("Interface closed.\n")
 	def do_shell(self,command):
 		"""shell <command>: run 'command' in shell"""
-		if stash is not None:
-			stash(command)
+		if _stash is not None:
+			_stash(command)
 		else:
 			p=os.popen(command)
 			content=p.read()
@@ -313,9 +478,9 @@ class McCmd(cmd.Cmd):
 		rfsi=self.FSIs[rfi]
 		wfsi=self.FSIs[wfi]
 		try:
-			self.stdout.write("Opening file to read... ")
+			self.stdout.write("Opening infile... ")
 			rf=rfsi.open(rfp,"rb")
-			self.stdout.write("Done.\nOpening file to write... ")
+			self.stdout.write("Done.\nOpening outfile... ")
 			wf=wfsi.open(wfp,"wb")
 			self.stdout.write("Done.\nCopying... ")
 			while True:
@@ -331,14 +496,18 @@ class McCmd(cmd.Cmd):
 			self.stdout.write("Error: {m}!\n".format(m=e.message))
 			return
 		finally:
+			self.stdout.write("Closing infile... ")
 			try:
 				rf.close()
-			except:
-				pass
+				self.stdout.write("Done.\n")
+			except Exception as e:
+				self.stdout.write("Error: {m}!\n".format(m=e.message))
+			self.stdout.write("Closing outfile... ")
 			try:
 				wf.close()
-			except:
-				pass
+				self.stdout.write("Done.\n")
+			except Exception as e:
+				self.stdout.write("Error: {m}!\n".format(m=e.message))
 	do_copy=do_cp
 	def do_mv(self,command):
 		"""mv <ri> <rf> <wi> <wn>: move file 'rf' from 'ri' to file 'wf' on 'wi'."""
@@ -368,8 +537,6 @@ class McCmd(cmd.Cmd):
 				if len(data)==0:
 					break
 				wf.write(data)
-			self.stdout.write("Done.\nDeleting Original... ")
-			rfsi.remove(rfp)
 			self.stdout.write("Done.\n")
 		except IsDir:
 			self.stdout.write("Error: expected a filepath!\n")
@@ -378,17 +545,28 @@ class McCmd(cmd.Cmd):
 			self.stdout.write("Error: {m}!\n".format(m=e.message))
 			return
 		finally:
+			self.stdout.write("Closing infile... ")
 			try:
 				rf.close()
-			except:
-				pass
+				self.stdout.write("Done.\n")
+			except Exception as e:
+				self.stdout.write("Error: {m}!\n".format(m=e.message))
+			self.stdout.write("Closing outfile... ")
 			try:
 				wf.close()
-			except:
-				pass
+				self.stdout.write("Done.\n")
+			except Exception as e:
+				self.stdout.write("Error: {m}!\n".format(m=e.message))
+				return
+			self.stdout.write("Deleting Original... ")
+			try: rfsi.remove(rfp)
+			except OperationFailure as e:
+				self.stdout.write("Error: {m}!\n".format(m=e.message))
+			else:
+				self.stdout.write("Done.\n")
 	do_move=do_mv
 	def do_cat(self,command):
-		"""ls <interface> <file> [--binary]: shows the content of target file on 'interface'.
+		"""cat <interface> <file> [--binary]: shows the content of target file on 'interface'.
 if --binary is specified, print all bytes directly.
 otherwise, print repr(content)[1:-1]
 """
@@ -448,7 +626,92 @@ nargs specifies the number of arguments, -1 means any number."""
 			raise ValueError("Unknown return type!")
 		fsi=self.FSIs[i]
 		return fsi,args
+	def help_usage(self,*args):
+		"""prints help about the usage."""
+		help="""USAGE
+===============
+This guide describes how to use mc.
+
+1.) using filesystem
+	First, you need to connect to a filesystem.
+	Use the 'connect'-command for this.
+	Usage:
+		connect <id> <fsi-name> [args [args ...]]
 		
+		'id' is a number used to identify the connection in commands.
+		'fsi-name' is the name of the FSI you want to use (e.g. 'local').
+		'args' is passed to the FSI and may contain server, username...
+	Example:
+		connect 1 local
+			>opens the local filesystem.
+			>you can later use this filesystem by passing 1 to commands.
+		connect 2 ftp ftp.example.org 21 YourName YourPswd -s
+			>connects to FTP-server ftp.example.org on port 21
+			>switch over to SFTP
+			>login as YourName with YourPswd
+			>you can later use this filesystem by passing 2 to commands.
+	If you want to get a list of connected filesystems, use 'connected'.
+	If you want to disconnect, use disconnect <id>.
+	If you want to get a list of aviable FSIs, use help fsis.
+	If you want to see help on a FSI, use help fsi_<name>
+2.) Using commands
+	After you are connected, you can use any aviable command.
+	To get a list of aviable commands, type '?' or 'help'.
+	To see the Usage of a command, use 'help <command>'.
+3.) Quitting
+	To quit, use the 'exit' command.
+4.) Tips
+	You can run shell commands while using mc:
+		!echo test
+		shell python
+"""
+		self.stdout.write(help+"\n")
+	def help_fsis(self,*args):
+		"""shows a list of aviable FSIs"""
+		av="""Aviable FSIs:
+local: the local filesystem
+ftp: a FTP-client
+"""
+		self.stdout.write(av+"\n")
+	def help_fsi_local(self,*args):
+		"""shows a list of aviable FSIs"""
+		av="""Help on FSI local:
+	The local filesystem.
+	USAGE:
+		'connect ' does not require any additional arguments.
+"""
+		self.stdout.write(av+"\n")
+	def help_fsi_ftp(self,*args):
+		"""shows a list of aviable FSIs"""
+		av="""Help on FSI ftp::
+	Use a FTP-server as a filesystem.
+	WARNING:
+		Unless explicitly told, this uses a non-secure connection.
+	INFO: 
+		FTP is designed as a human-readable protocol.
+		Different servers may use different commands and different responses.
+		This means that the 'ftp'-FSI may not work with all FTP-servers.
+	USAGE:
+		connect <ID> ftp <host> [port] [mode or user] [pswd] [mode]
+		
+		'ID': see 'help connect'
+		'host': the IP of the FTP-server
+		'port': port of the FTP-server. 21 if not specified.
+		'mode or user':
+			if len(args)==5:
+				the type of the connection. see mode
+			else:
+				the username to use for login.
+				defaults to 'anonymous'
+		'pswd': the password to use for login. Defaults to 'anonymous@'
+		'mode':
+			one of:
+				'-s': use a secure connection.
+				'-n': dont use a secure connection (default)
+				'-d': use a secure connection and set debug to max.
+		
+"""
+		self.stdout.write(av+"\n")
 
 if __name__=="__main__":
 	McCmd().cmdloop()
