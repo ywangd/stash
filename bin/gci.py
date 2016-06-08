76134f4dd4e5e170e61c12720eba54e46359b725
bennr01 <bennr01@users.noreply.github.com>
2016-06-08 19:31:06 +0200

    gci.py: py3 compatibility + colors
    The gci-command now uses colors.
    For better py3-compatibility, gci now uses sys.stdout.write() instead of print. (I wasnt able to test the compatibility yet because i just applied for the beta-test).

--- bin/gci.py
+++ bin/gci.py
@@ -1,8 +1,10 @@
 # coding: utf-8
-"""interface to pythons built-in garbage collector
+"""Interface to pythons built-in garbage collector
 
-Warning: this command may crash StaSh. Only use it if you know what dou are doing!"""
+Warning: this command may crash StaSh. Only use it if you know what you are doing!"""
 import gc,argparse,sys
+
+_stash=globals()["_stash"]
 
 def main():
 	parser=argparse.ArgumentParser(description="access to pythons built-in garbage collector")
@@ -16,46 +18,52 @@ def main():
 	elif ns.command=="collect":
 		gc.collect()
 	elif ns.command=="status":
-		print "GC enabled:              {s}".format(s=gc.isenabled())
+		if gc.isenabled():
+			gcs=_stash.text_color("Enabled","green")
+		else:
+			gcs=_stash.text_color("Disabled","red")
+		sys.stdout.write("GC status:               {s}\n".format(s=gcs))
 		tracked=gc.get_objects()
 		n=len(tracked)
-		print "Tracked objects:         {n}".format(n=n)
+		sys.stdout.write("Tracked objects:         {n}\n".format(n=n))
 		size=sum([sys.getsizeof(e) for e in tracked])
 		del tracked#this list may be big, better delete it
-		print "Size of tracked objects: {s} bytes".format(s=size)
-		print "Garbage:                 {n}".format(n=len(gc.garbage))
+		sys.stdout.write("Size of tracked objects: {s} bytes\n".format(s=size))
+		sys.stdout.write("Garbage:                 {n}\n".format(n=len(gc.garbage)))
 		gsize=sum([sys.getsizeof(e) for e in gc.garbage])
-		print "Size of garbage:         {s} bytes".format(s=gsize)
-		print "Debug:                   {d}".format(d=gc.get_debug())
+		sys.stdout.write("Size of garbage:         {s} bytes\n".format(s=gsize))
+		sys.stdout.write("Debug:                   {d}\n".format(d=gc.get_debug()))
 	elif ns.command=="threshold":
 		if len(ns.args)==0:
-			print "Threshold:\n   G1: {}\n   G2: {}\n   G3: {}".format(*gc.get_threshold())
+			sys.stdout.write("Threshold:\n   G1: {}\n   G2: {}\n   G3: {}\n".format(*gc.get_threshold()))
 		elif len(ns.args)>3:
-			print "Error: to many arguments for threshold!"
+			errmsg=_stash.text_color("Error: to many arguments for threshold!\n","red")
+			sys.stdout.write(errmsg)
 			sys.exit(1)
 		else:
 			try:
 				ts=tuple([int(e) for e in ns.args])
 			except ValueError:
-				print "Error: expected arguments to be integer!"
+				errmsg=_stash.text_color("Error: expected arguments to be integer!\n","red")
+				sys.stdout.write(errmsg)
 				sys.exit(1)
 			gc.set_threshold(*ts)
 	elif ns.command=="debug":
 		if len(ns.args)==0:
-			print "Debug: {d}".format(d=gc.get_debug())
+			sys.stdout.write("Debug: {d}\n".format(d=gc.get_debug()))
 		elif len(ns.args)==1:
 			try:
 				flag=int(ns.args[0])
 			except ValueError:
-				print "Error: expected argument to be an integer!"
+				sys.stdout.write(_stash.text_color("Error: expected argument to be an integer!\n","red"))
 				sys.exit(1)
 			gc.set_debug(flag)
 		else:
-			print "Error: expected exactly one argument for threshold!"
+			sys.stdout.write(_stash.text_color("Error: expected exactly one argument for debug!\n","red"))
 			sys.exit(1)
 	elif ns.command=="break":
 		if len(gc.garbage)==0:
-			print "Error: No Garbage found!"
+			sys.stdout.write(_stash.text_color("Error: No Garbage found!\n","red"))
 			sys.exit(1)
 		else:
 			for k in dir(garbage[0]):
