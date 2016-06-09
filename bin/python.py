b63b83a467b1ed4a52964d1dc3e1c915237c411e
Yang Wang <ywangd@gmail.com>
2016-06-09 19:15:01 +1000

    Optimized -h handling of python command.

--- bin/python.py
+++ bin/python.py
@@ -18,6 +18,13 @@ import argparse
 import code
 import __builtin__
 
+args = sys.argv[1:]
+
+passing_h = False
+if '-h' in args and len(args) > 1:
+    args.remove('-h')
+    passing_h = True
+
 ap = argparse.ArgumentParser()
 
 group = ap.add_mutually_exclusive_group()
@@ -30,9 +37,13 @@ group.add_argument('-c', '--cmd',
 
 ap.add_argument('args_to_pass',
                 metavar='[file] args_to_pass',
+                default=[],
                 nargs=argparse.REMAINDER,
                 help='Python script and arguments')
-ns = ap.parse_args()
+
+ns = ap.parse_args(args)
+if passing_h:
+    ns.args_to_pass.append('-h')
 
 if ns.module:
     sys.argv = [ns.module] + ns.args_to_pass
