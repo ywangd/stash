25fffa5012602a47c0e7387ed86360b6be852e4e
bennr01 <bennr01@users.noreply.github.com>
2016-06-10 16:57:17 +0200

    selfupdate.py: added repo owner in args
    When running a selfupdate, you can now choose from which users repo/fork you want to update.

--- bin/selfupdate.py
+++ bin/selfupdate.py
@@ -17,14 +17,15 @@ from argparse import ArgumentParser
 
 _stash = globals()['_stash']
 
-URL_BASE = 'https://raw.githubusercontent.com/ywangd/stash'
+URL_BASE = 'https://raw.githubusercontent.com/{owner}/stash'
+DEFAULT_REPO_OWNER="ywangd"
 
 
 class UpdateError(Exception):
     pass
 
 
-def get_remote_version(branch):
+def get_remote_version(owner, branch):
     """
     Get the version string for the given branch from the remote repo
     :param branch:
@@ -32,12 +33,16 @@ def get_remote_version(branch):
     """
     import ast
 
-    url = '%s/%s/stash.py?q=%s' % (URL_BASE, branch, randint(1, 999999))
+    url = '%s/%s/stash.py?q=%s' % (URL_BASE.format(owner=owner), branch, randint(1, 999999))
 
     try:
-        lines = requests.get(url).text.splitlines()
+        req = requests.get(url)
+        lines = req.text.splitlines()
     except:
         raise UpdateError('Network error')
+    else:
+        if req.status_code==404:
+            raise UpdateError('Repository/branch not found')
 
     for line in lines:
         if line.startswith('__version__'):
@@ -53,6 +58,7 @@ def main(args):
 
     ap = ArgumentParser()
     ap.add_argument('branch', nargs='?', help='the branch to update to')
+    ap.add_argument('repository', nargs='?', help='the user owning the target repository to update to')
     ap.add_argument('-n', '--check', action='store_true', help='check for update only')
     ap.add_argument('-f', '--force', action='store_true', help='update without checking for new version')
     ns = ap.parse_args(args)
@@ -61,10 +67,15 @@ def main(args):
         SELFUPDATE_BRANCH = ns.branch
     else:
         SELFUPDATE_BRANCH = os.environ.get('SELFUPDATE_BRANCH', 'master')
+    
+    if ns.repository is not None:
+        SELFUPDATE_REPO = ns.repository
+    else:
+        SELFUPDATE_REPO = os.environ.get('SELFUPDATE_REPOSITORY',DEFAULT_REPO_OWNER)
 
     print _stash.text_style('Running selfupdate ...',
                             {'color': 'yellow', 'traits': ['bold']})
-    print u'%s: %s' % (_stash.text_bold('Branch'), SELFUPDATE_BRANCH)
+    print u'%s: %s/%s' % (_stash.text_bold('Branch'), SELFUPDATE_REPO, SELFUPDATE_BRANCH)
 
     has_update = True
     # Check for update if it is not forced updating
@@ -72,7 +83,7 @@ def main(args):
         local_version = globals()['_stash'].__version__
         try:
             print 'Checking for new version ...'
-            remote_version = get_remote_version(SELFUPDATE_BRANCH)
+            remote_version = get_remote_version(SELFUPDATE_REPO, SELFUPDATE_BRANCH)
 
             if StrictVersion(remote_version) > StrictVersion(local_version):
                 print 'New version available: %s' % remote_version
@@ -87,16 +98,16 @@ def main(args):
     # Perform update if new version is available and not just checking only
     if not ns.check and has_update:
 
-        url = '%s/%s/getstash.py' % (URL_BASE, SELFUPDATE_BRANCH)
+        url = '%s/%s/getstash.py' % (URL_BASE.format(owner=SELFUPDATE_REPO), SELFUPDATE_BRANCH)
         print u'%s: %s' % (_stash.text_bold('Url'),
                            _stash.text_style(url, {'color': 'blue', 'traits': ['underline']}))
 
         try:
             exec requests.get(
-                '%s/%s/getstash.py?q=%s' % (URL_BASE,
+                '%s/%s/getstash.py?q=%s' % (URL_BASE.format(owner=SELFUPDATE_REPO),
                                             SELFUPDATE_BRANCH,
                                             randint(1, 999999))
-            ).text in {'_IS_UPDATE': True, '_br': SELFUPDATE_BRANCH}
+            ).text in {'_IS_UPDATE': True, '_br': SELFUPDATE_BRANCH,'_repo':SELFUPDATE_REPO}
             print _stash.text_color('Update completed.', 'green')
             print _stash.text_color('Please restart Pythonista to ensure changes becoming effective.', 'green')
 
