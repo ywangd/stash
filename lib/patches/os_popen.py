cfa6e507afd8e51eb9bf94bff1a5dca5043ababe
bennr01 <bennr01@users.noreply.github.com>
2016-07-12 14:49:48 +0200

    os_popen.py: code cleanup
    cleaned some code up

--- lib/patches/os_popen.py
+++ lib/patches/os_popen.py
@@ -10,13 +10,15 @@ class _PipeEndpoint(object):
 		self.__pipe = pipe
 	
 	def __getattr__(self, name):
+		"""return attribute name of the pipe."""
 		return getattr(self.__pipe, name)
 	
 	def __hasattr__(self, name):
+		"""checks wether the pioe has a attribute called name."""
 		return hasattr(self.__pipe, name)
 	
 	def __repr__(self):
-		return repr(self.__pipe.fileno()) # debug
+		"""returns the representation of the pipe."""
 		return repr(self.__pipe)
 	
 	def __del__(self):
@@ -25,12 +27,11 @@ class _PipeEndpoint(object):
 	
 	def close(self):
 		"""closes the pipe."""
-		print "closing: ", self.__pipe.fileno()
 		try:
 			os.close(self.__pipe.fileno())
 		except OSError:
 			pass
-		ec = self.__root._get_exit_code()
+		ec = self.__root.get_exit_code(wait=True)
 		if ec == 0:
 			return None  # see os.popen
 		else:
@@ -40,76 +41,81 @@ class _PipeEndpoint(object):
 class _PopenCmd(object):
 	"""This class handles the command processing."""
 	def __init__(self, cmd, mode, bufsize, shared_eo=False):
-		self.__cmd = cmd
-		self.__mode = mode
-		self.__bufsize = bufsize
-		self.__fds = []
-		self.__exitstatus = None
-		self.__worker = None
-		self.__shared_eo = shared_eo
-		self.__chinr, self.__chinw = self.__create_pipe(wbuf=bufsize)
-		self.__choutr, self.__choutw = self.__create_pipe(rbuf=bufsize)
-		self.__killer = 0
+		self.cmd = cmd
+		self.mode = mode
+		self.bufsize = bufsize
+		self.fds = []
+		self.exitstatus = None
+		self.worker = None
+		self.shared_eo = shared_eo
+		self.chinr, self.chinw = self.create_pipe(wbuf=bufsize)
+		self.choutr, self.choutw = self.create_pipe(rbuf=bufsize)
+		self.killer = 0
 		if shared_eo:
-			self.__cherrr, self.__cherrw = self.__choutr, self.__choutw
+			self.cherrr, self.cherrw = self.choutr, self.choutw
 		else:
-			self.__cherrr, self.__cherrw = self.__create_pipe(rbuf=bufsize)
+			self.cherrr, self.cherrw = self.create_pipe(rbuf=bufsize)
 	
-	def _get_pipes(self):
+	def get_pipes(self):
 		"""returns the pipes."""
-		if not self.__shared_eo:
+		if not self.shared_eo:
 			return (
-				_PipeEndpoint(self, self.__chinw),
-				_PipeEndpoint(self, self.__choutr),
-				_PipeEndpoint(self, self.__cherrr)
+				_PipeEndpoint(self, self.chinw),
+				_PipeEndpoint(self, self.choutr),
+				_PipeEndpoint(self, self.cherrr)
 				)
 		else:
 			return (
-				_PipeEndpoint(self, self.__chinw),
-				_PipeEndpoint(self, self.__choutr)
+				_PipeEndpoint(self, self.chinw),
+				_PipeEndpoint(self, self.choutr)
 				)
 	
-	def __close_fds(self):
+	def close_fds(self):
 		"""close all fds."""
-		for fd in self.__fds:
+		for fd in self.fds:
 			try:
 				os.close(fd)
 			except os.OSError:
 				pass
 	
-	def __create_pipe(self, rbuf=0, wbuf=0):
+	def create_pipe(self, rbuf=0, wbuf=0):
 		"""creates a pipe. returns (readpipe, writepipe)"""
 		rfd, wfd = os.pipe()
-		self.__fds += [rfd, wfd]
+		self.fds += [rfd, wfd]
 		rf, wf = os.fdopen(rfd, "rb", rbuf), os.fdopen(wfd, "wb", wbuf)
 		return rf, wf
 	
-	def _exec(self):
+	def run(self):
 		"""runs the command."""
-		self.__worker = _stash(
-			input_=self.__cmd,
+		self.worker = _stash(
+			input_=self.cmd,
 			persistent_level=2,
 			is_background=False,
 			wait=False,
 			add_to_history=None,
-			final_ins=self.__chinr,
-			final_outs=self.__choutw,
-			final_errs=self.__cherrw
+			final_ins=self.chinr,
+			final_outs=self.choutw,
+			final_errs=self.cherrw
 			)
 	
-	def _get_exit_code(self):
-		if self.__worker is not None:
-			self.__worker.join()
-			es = self.__worker.state.return_value
-			return (es * self.__killer) + es
-		return 0
+	def get_exit_code(self, wait=True):
+		"""returns the exitcode. 
+		If wait is False and the worker has not finishef yet, return None."""
+		if self.worker is not None:
+			if wait:
+				self.worker.join()
+			elif self.worker.status() != self.worker.STOPPED:
+				return None
+			es = self.worker.state.return_value
+			return (es * 256) + self.killer
+		raise RuntimeError("get_exit_code() called before run()!")
 	
 
 def popen(cmd, mode="r", bufsize=0):
 	"""Execute cmd as a sub-process and return the file objects (child_stdin, child_stdout)."""
 	cmd = _PopenCmd(cmd, mode, bufsize, shared_eo=False)
-	pipes = cmd._get_pipes()
-	cmd._exec()
+	pipes = cmd.get_pipes()
+	cmd.run()
 	if mode == "r":
 		return pipes[1]
 	elif mode == "w":
@@ -119,24 +125,24 @@ def popen(cmd, mode="r", bufsize=0):
 def popen2(cmd, mode="r", bufsize=0):
 	"""Execute cmd as a sub-process and return the file objects (child_stdin, child_stdout)."""
 	cmd = _PopenCmd(cmd, mode, bufsize, shared_eo=False)
-	pipes = cmd._get_pipes()
-	cmd._exec()
+	pipes = cmd.get_pipes()
+	cmd.run()
 	return pipes[0], pipes[1]
 
 
 def popen3(cmd, mode="r", bufsize=0):
 	"""Execute cmd as a sub-process and return the file objects (child_stdin, child_stdout, child_stderr)."""
 	cmd = _PopenCmd(cmd, mode, bufsize, shared_eo=False)
-	pipes = cmd._get_pipes()
-	cmd._exec()
+	pipes = cmd.get_pipes()
+	cmd.run()
 	return pipes[0], pipes[1], pipes[2]
 
 
 def popen4(cmd, mode="r", bufsize=0):
 	"""Execute cmd as a sub-process and return the file objects (child_stdin, child_stdout_and_stderr)."""
 	cmd = _PopenCmd(cmd, mode, bufsize, shared_eo=True)
-	pipes = cmd._get_pipes()
-	cmd._exec()
+	pipes = cmd.get_pipes()
+	cmd.run()
 	return pipes[0], pipes[1]
 
 
@@ -144,7 +150,7 @@ if __name__ == "__main__":
 	# testcode
 	import select
 	cmd = raw_input("cmd: ")
-	i, o = popen3(cmd)
+	i, o = popen4(cmd)
 	rs = [o]
 	ws = [i]
 	while True:
