e49b76adb4b906ca288c67d8f7dbb4bd1b912618
bennr01 <benjamin99.vogt@web.de>
2018-05-31 12:33:37 +0200

    add cat tests

--- bin/cat.py
+++ bin/cat.py
@@ -11,7 +11,7 @@ import sys
 import fileinput
 def filter_non_printable(s):
     return ''.join([c if (31 < ord(c) < 127 or c in "\n\r\t\b") else ' ' for c in s])
-    
+
 def main(args):
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument("files", action="store", nargs="*",
@@ -25,7 +25,7 @@ def main(args):
         for line in fileinput.input(ns.files):
             print(filter_non_printable(line), end='')
     except Exception as e:
-        print('cat :%s' % str(e))
+        print('cat: %s' % str(e))
         status = 1
     finally:
         fileinput.close()
