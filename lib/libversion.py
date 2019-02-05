8a35180b51f194a1799e03269a0a07abc249cd27
bennr01 <benjamin99.vogt@web.de>
2019-02-05 14:23:24 +0100

    move version comparsion logic into seperate class; use this class for VersionSpecifier matches

--- lib/libversion.py
+++ lib/libversion.py
@@ -14,6 +14,101 @@ RELEASE_TYPE_PRIORITIES = {
 }
 
 
+def _parse_version(vs):
+    """
+    Parse a version string, e.g. '2!3.0.1.rc2.dev3'
+    :param vs: version to parse
+    :type vs: str
+    :return: a dict containing the fragments about the version
+    :rtype: dict
+    """
+    # NOTE: the below code may be a bit messy, because it was rewritten multiple times and then repurposed from a sort-function to a parsing-function
+    # version scheme (PEP440): [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
+    # convert to lowercase, since all versions must be case insenstive (PEP440)
+    e = vs.lower()
+    # extract information from
+    if "!" in e:
+        # read epoch
+        es = e[:e.find("!")]
+        e = e[e.find("!") + 1:]
+        epoch = int(es)
+    else:
+        # default epoch is 0
+        epoch = 0
+    # parse version tuple
+    veis = re.search("[^0-9\\.]", e)
+    if veis is None:
+        # no non-digits
+        vei = len(e)
+    else:
+        vei = veis.start()
+    vstr = e[:vei]
+    while vstr.endswith("."):
+        # remove trailing '.'
+        vstr = vstr[:-1]
+    splitted = vstr.split(".")
+    verparts = []
+    for v in splitted:
+        verparts.append(int(v))
+    # parse post release
+    rtstr = e[vei:]
+    if "post" in rtstr:
+        postrnstr = rtstr[rtstr.find("post") + 4:]
+        postrnres = re.search("[0-9]*", postrnstr)
+        if postrnres is None or postrnres.end() == 0:
+            # PEP440: implicit post version 0
+            subpriority = 0
+        else:
+            subpriority = int(postrnstr[:postrnres.end()])
+        rtstr = rtstr[:rtstr.find("post") - 1]
+        is_post = True
+    else:
+        subpriority = 0
+        is_post = False
+    # parse release type
+    rtype = None
+    if len(rtstr) == 0:
+        rtype = None
+    elif rtstr[0] in ("a", "b"):
+        rtype = rtstr[0]
+    elif rtstr.startswith("rc"):
+        rtype = "rc"
+    # parse number of release
+    if rtype is None:
+        # not needed
+        rsubpriority = 0
+    else:
+        # 1. strip rtype
+        rtps = rtstr[len(rtype):]
+        # 2. extract until non-digit
+        rtpsr = re.search("[0-9]*", rtps)
+        if rtpsr is None or rtpsr.end() == 0:
+            # no number
+            rsubpriority = 0
+        else:
+            rsubpriority = int(rtps[:rtpsr.end()])
+    # extract dev release information
+    devr = re.search("\\.?dev[0-9]*", e)
+    if devr is None:
+        isdev = False
+        devnum = 0
+    else:
+        isdev = True
+        devns = e[devr.start() + 4:]  # 4: 1 for '.'; 3 for 'dev'
+        if len(devns) == 0:
+            devnum = 0
+        else:
+            devnum = int(devns)
+    return {
+        "epoch": epoch,
+        "versiontuple": tuple(verparts),
+        "rtype": rtype,
+        "subversion": rsubpriority,
+        "postrelease": (subpriority if is_post else None),
+        "devrelease": (devnum if isdev else None),
+        }
+
+
 def sort_versions(versionlist):
     """
     Return a list containing the versions in versionlist, starting with the highest version.
@@ -22,94 +117,114 @@ def sort_versions(versionlist):
     :return: the sorted list
     :rtype: list of str
     """
-    # version scheme (PEP440): [N!]N(.N)*[{a|b|rc}N][.postN][.devN]
-    # order:
-    # 1. epoch
-    # 2. release version
-    # 3. postrelease > release > prerelease
-    # 4. postrelease#
-    # 5. non-dev > dev
-    # 6. dev#
-    def sortf(e):
-        """extract the key for the search"""
-        # convert to lowercase, since all versions must be case insenstive (PEP440)
-        e = e.lower()
-        # extract information from
-        if "!" in e:
-            # read epoch
-            es = e[:e.find("!")]
-            e = e[e.find("!") + 1:]
-            epoch = int(es)
+    return sorted(versionlist, sortkey=lambda s: Version.parse(s) if isinstance(s, str) else s, reverse=True)
+
+
+class Version(object):
+    """
+    This class represents a version. It is mainly used for version comparsion.
+    """
+    TYPE_NORMAL = None
+    TYPE_ALPHA = "a"
+    TYPE_BETA = "b"
+    TYPE_RELEASE_CANDIDATE = "rc"
+
+    RELEASE_TYPE_PRIORITIES = {
+        # priority of a release type. greate => higher priority
+        TYPE_NORMAL: 3,   # no release type
+        TYPE_ALPHA: 0,    # alpha post release
+        TYPE_BETA: 1,    # beta post release
+        TYPE_RELEASE_CANDIDATE: 2,   # release candidate
+    }
+
+    def __init__(self, epoch=0, versiontuple=(), rtype=None, subversion=0, postrelease=None, devrelease=None):
+        assert isinstance(epoch, int)
+        assert isinstance(versiontuple, tuple)
+        assert isinstance(rtype, str) or rtype is None
+        assert isinstance(subversion, int)
+        assert isinstance(postrelease, int) or postrelease is None
+        assert isinstance(devrelease, int) or devrelease is None
+        self.epoch = epoch
+        self.versiontuple = versiontuple
+        self.rtype = rtype
+        self.subversion = subversion
+        self.postrelease = postrelease
+        self.devrelease = devrelease
+
+    @classmethod
+    def parse(cls, s):
+        """
+        Parse a versionstring and return a Version() of it.
+        :param s: string to parse
+        :type s: str
+        :return: a Version() instance describing the parsed string
+        :rtype: Version
+        """
+        if isinstance(s, cls):
+            # s is already a Version
+            return s
+        parsed = _parse_version(s)
+        return Version(**parsed)
+
+    @property
+    def is_postrelease(self):
+        """whether this version is a postrelease or not"""
+        return self.postrelease is not None
+
+    @property
+    def is_devrelease(self):
+        """whether this version is a devrelease or not"""
+        return self.devrelease is not None
+
+    def _get_sortkey(self):
+        """
+        Return a value which can be used to compare two versions.
+        Sort order:
+        1. epoch
+        2. release version
+        3. postrelease > release > prerelease
+        4. postrelease#
+        5. non-dev > dev
+        6. dev#
+
+        :return: a value which can be used for comparing this version to another version
+        :rtype: tuple
+        """
+        rpriority = self.RELEASE_TYPE_PRIORITIES.get(self.rtype, 0)
+        return (self.epoch, self.versiontuple, rpriority, self.subversion, self.is_postrelease, self.postrelease, not self.is_devrelease, self.devrelease)
+
+    def __cmp__(self, other):
+        """
+        Compare to another object
+        """
+        if isinstance(other, self.__class__):
+            return cmp(self._get_sortkey(), other._get_sortkey())
         else:
-            # default epoch is 0
-            epoch = 0
-        # parse version tuple
-        veis = re.search("[^0-9\\.]", e)
-        if veis is None:
-            # no non-digits
-            vei = len(e)
-        else:
-            vei = veis.start()
-        vstr = e[:vei]
-        while vstr.endswith("."):
-            # remove trailing '.'
-            vstr = vstr[:-1]
-        splitted = vstr.split(".")
-        verparts = []
-        for v in splitted:
-            verparts.append(int(v))
-        # parse post release
-        rtstr = e[vei:]
-        if "post" in rtstr:
-            try:
-                subpriority = int(rtstr[rtstr.find("post") + 4:])
-            except ValueError:
-                # PEP440: implicit post version 0
-                subpriority = 0
-            rtstr = rtstr[:rtstr.find("post") - 1]
-            is_post = True
-        else:
-            subpriority = 0
-            is_post = False
-        # parse release type
-        rtype = None
-        if len(rtstr) == 0:
-            rtype = None
-        elif rtstr[0] in ("a", "b"):
-            rtype = rtstr[0]
-        elif rtstr.startswith("rc"):
-            rtype = "rc"
-        # parse number of release
-        if rtype is None:
-            # not needed
-            rsubpriority = 0
-        else:
-            # 1. strip rtype
-            rtps = rtstr[len(rtype):]
-            # 2. extract until non-digit
-            rtpsr = re.search("[0-9]*", rtps)
-            if rtpsr is None or rtpsr.end() == 0:
-                # no number
-                rsubpriority = 0
-            else:
-                rsubpriority = int(rtps[:rtpsr.end()])
-        rpriority = RELEASE_TYPE_PRIORITIES.get(rtype, 0)  # unknown -> be skeptical
-        # extract dev release information
-        devr = re.search("\\.?dev[0-9]*", rtstr)
-        if devr is None:
-            isdev = False
-            devnum = 0
-        else:
-            isdev = True
-            devns = rtstr[devr.start() + 4:]  # 4: 1 for '.'; 3 for 'dev'
-            if len(devns) == 0:
-                devnum = 0
-            else:
-                devnum = int(devns)
-        # below is the search key (=value by which the version list will be ordered)
-        # comparsion *should* be from i=0 to i=-1
-        return (epoch, tuple(verparts), rpriority, rsubpriority, is_post, subpriority, not isdev, devnum)
-    return sorted(versionlist, key=sortf, reverse=True)
+            return 1  # assume greater than non-versions
+
+    def __str__(self):
+        """return a string representation of this version"""
+        version = ".".join([str(e) for e in self.versiontuple])  # base version
+        # epoch
+        if self.epoch > 0:
+            version = str(self.epoch) + "!" + version
+        # release type
+        if self.rtype is not None:
+            version += "." + self.rtype
+            if self.subversion > 0:
+                version += str(self.subversion)
+        # postrelease
+        if self.is_postrelease:
+            version += ".post"
+            if self.postrelease > 0:
+                version += str(self.postrelease)
+        # devrelease
+        if self.is_devrelease:
+            version += ".dev"
+            if self.devrelease > 0:
+                version += str(self.devrelease)
+        # done
+        return version
 
 
 class VersionSpecifier(object):
@@ -175,4 +290,4 @@ class VersionSpecifier(object):
         :return: whether the version is allowed or not
         :rtype: boolean
         """
-        return all([op(version, ver) for op, ver in self.specs])
+        return all([op(Version.parse(version), Version.parse(ver)) for op, ver in self.specs])
--- tests/misc/test_libversion.py
+++ tests/misc/test_libversion.py
@@ -27,7 +27,7 @@ class LibVersionTests(StashTestCase):
             ("tgt > 3.0.0", "tgt", [(operator.gt, "3.0.0")]),
             ("tne != 7.0.0", "tne", [(operator.ne, "7.0.0")]),
             ("pkg_b (< 0.7.0", "pkg_b", [(operator.lt, "0.7.0")]),
-            ("nondigitver <= 1.5.3b", [(operator.le, "1.5.3b")]),
+            ("nondigitver <= 1.5.3b", "nondigitver", [(operator.le, "1.5.3b")]),
         ]
         for req, pkg, spec in to_test:
             name, ver_spec = self.stash.libversion.VersionSpecifier.parse_requirement(req)
@@ -69,3 +69,71 @@ class LibVersionTests(StashTestCase):
         expected = ["1!0.5.0", "1.0.0.post1", "1.0.0", "1.0.0rc", "1.0.0b", "1.0.0a2", "1.0.0a", "1.0.0a.dev3", "1.0.0a.dev2", "1.0.0a.dev1", "0.5.0"]
         sortedres = self.stash.libversion.sort_versions(raw)
         self.assertEqual(sortedres, expected)
+
+    def test_version_parse(self):
+        """test 'libversion.Version.parse()''"""
+        to_test = [
+            # format: (s, {key_to_check: expected_value, ...})
+            ("1.2.3", {"epoch": 0, "versiontuple": (1, 2, 3), "is_postrelease": False}),
+            ("1!2.3", {"epoch": 1, "versiontuple": (2, 3), "is_devrelease": False}),
+            ("5.5.4a.post5", {"versiontuple": (5, 5, 4), "rtype": self.stash.libversion.Version.TYPE_ALPHA, "is_postrelease": True, "postrelease": 5}),
+            ("0.0.1rc5.dev7", {"versiontuple": (0, 0, 1), "rtype": self.stash.libversion.Version.TYPE_RELEASE_CANDIDATE, "subversion": 5, "is_devrelease": True, "devrelease": 7, "is_postrelease": False}),
+            ("0.8.4.post.dev", {"veriontuple": (0, 8, 4), "is_postrelease": True, "postrelease": 0, "is_devrelease": True}),
+        ]
+        for vs, ea in to_test:
+            version = self.stash.libversion.Version.parse(vs)
+            self.assertIsInstance(version, self.stash.libversion.Version)
+            for ean in ea.keys():
+                eav = ea[ean]
+                assert hasattr(version, ean)
+                self.assertEqual(getattr(version, ean), eav)
+
+    def test_version_cmp(self):
+        """test comparsion of 'libversion.Version()'-instances"""
+        to_test = [
+            # format: (vs1, op, vs2, res)
+            # testdata for general equality
+            ("1.0.0", operator.eq, "1.0.0", True),
+            ("1.0.0", operator.eq, "0!1.0.0", True),
+            ("1.0.0", operator.eq, "1!1.0.0", False),
+            ("1.0.0", operator.eq, "1.0.0.post", False),
+            ("1.0.0", operator.eq, "1.0.0a", False),
+            ("1.0.0", operator.eq, "1.0.0b", False),
+            ("1.0.0.post1", operator.eq, "1.0.0.post2", False),
+            ("1.0.0.post", operator.eq, "1.0.0.post1", True),
+            ("1.0.0.post", operator.eq, "1.0.0.dev", False),
+            # testdata for main version comparsion
+            ("1.2.3", operator.eq, "1.5.0", False),
+            ("2.0.3", operator.gt, "1.9.7", True),
+            ("1.9.7", operator.gt, "2.0.3", False),
+            ("1.9.7", operator.lt, "2.0.3", True),
+            ("1.9.7", operator.lt, "1.9.7", False),
+            ("1.9.7", operator.le, "1.9.7", True),
+            ("2.4.9", operator.gt, "11.0.5", False),
+            ("2.4.9", operator.gt, "1.0.5", True),
+            ("2.4.9", operator.gt, "2.5.1", False),
+            ("2.5.2", operator.gt, "2.5.1", True),
+            # testdata for rtype comparsion
+            ("1.0.0", operator.eq, "1.0.0a", False),
+            ("1.0.0", operator.eq, "1.0.0b", False),
+            ("1.0.0", operator.eq, "1.0.0rc", False),
+            ("1.0.0a", operator.eq, "1.0.0b", False),
+            ("1.0.0a", operator.eq, "1.0.0rc", False),
+            ("1.0.0b", operator.eq, "1.0.0rc", False),
+            ("1.0.0", operator.gt, "1.0.0rc", True),
+            ("1.0.0rc", operator.gt, "1.0.0b", True),
+            ("1.0.0b", operator.gt, "1.0.0a", True),
+            ("1.0.0", operator.gt, "1.0.0b", True),
+            ("1.0.0", operator.gt, "1.0.0a", True),
+            ("1.0.0rc", operator.gt, "1.0.0a", True),
+            # testdata for dev version comparsion
+            ("1.0.0", operator.gt, "1.0.0.dev", True),
+            ("1.0.0", operator.gt, "1.0.0.dev1000", True),
+            ("1.0.0.dev", operator.gt, "1.0.0", False),
+            ("1.0.0.dev2", operator.gt, "1.0.0.dev", True),
+        ]
+        for vs1, op, vs2, expected in to_test:
+            v1 = self.stash.libversion.Version.parse(vs1)
+            v2 = self.stash.libversion.Version.parse(vs2)
+            self.assertEqual(op(v1, v2), expected)
+
