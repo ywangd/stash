8dda6fcec1370f4325005bbc3456b2f4923c9aaf
bennr01 <benjamin99.vogt@web.de>
2019-06-08 09:46:22 +0200

    stashutils.extensions: now handle unicode properly; fixes #378

--- lib/stashutils/extensions.py
+++ lib/stashutils/extensions.py
@@ -2,6 +2,7 @@
 """This module defines functions to interact with stash extensions."""
 import os
 import shutil
+import io
 
 from stash.system.shcommon import _STASH_EXTENSION_BIN_PATH as EBP
 from stash.system.shcommon import _STASH_EXTENSION_MAN_PATH as EMP
@@ -18,27 +19,31 @@ load_from_dir = load_from_dir
 
 def create_file(dest, content):
     """
-	creates a file at dest with content.
-	If content is a string or unicode, use it as the content.
-	Otherwise, use content.read() as the content.
-	"""
+    Creates a file at dest with content.
+    If content is a string or unicode, use it as the content.
+    Otherwise, use content.read() as the content.
+    """
     if not isinstance(content, (binary_type, text_type)):
         content = content.read()
     parent = os.path.dirname(dest)
     if not os.path.exists(parent):
         os.makedirs(parent)
-    with open(dest, "wb") as f:
-        f.write(content)
+    if isinstance(content, binary_type):
+        with io.open(dest, "wb") as f:
+            f.write(content)
+    elif isinstance(content, text_type):
+        with io.open(dest, "w", encoding="utf-8") as f:
+            f.write(content)
     return dest
 
 
 def create_page(name, content):
     """
-	creates a manpage with name filled with content.
-	If content is a list or tuple, instead create a dir and fill it with pages
-	created from the elements of this list.
-	The list should consist of tuples of (ending, content)
-	"""
+    Creates a manpage with name filled with content.
+    If content is a list or tuple, instead create a dir and fill it with pages
+    created from the elements of this list.
+    The list should consist of tuples of (ending, content)
+    """
     path = os.path.join(EMP, name)
     if isinstance(content, (list, tuple)):
         # create a bunch of pages
