3c09efe81c6150db76a2b00a98ab4d9dfc9fc3d4
SnowleopardXI <69493681+SnowleopardXI@users.noreply.github.com>
2020-08-11 16:15:11 +0800

    Update Shortlink.py

--- bin/Shortlink.py
+++ bin/Shortlink.py
@@ -2,12 +2,13 @@ import requests
 import argparse
 import sys
 def main(args):
-    print("-url {0}".format(args.inp))
+    print("-url {0}".format(args.url))
 def url():
     long_url = inpu
     querystring = {"url":long_url}
     url = "http://suo.im/api.php"
-    response = requests.request("GET", url, params=querystring)
+    param={"url":"inpu","key":"5f3252d6b1b63c5aeec52d18@0e52d0179c9d30bfbc945b11581a0f76"}
+    response = requests.request("GET", url, params=param)
     print(response.text)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(usage="it's usage tip.", description="help info.")
