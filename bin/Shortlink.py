d253435f417745add15f923904cafd5753f74db8
SnowleopardXI <69493681+SnowleopardXI@users.noreply.github.com>
2020-08-11 16:03:23 +0800

    Update Shortlink.py

--- bin/Shortlink.py
+++ bin/Shortlink.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     parser.add_argument("-url", type=str, required=True, help="The Origin URL.")
     args = parser.parse_args()
     main(args)
-    inp = args.id
+    inp = args.inp
     url()
