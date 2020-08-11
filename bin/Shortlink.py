a7c1fc3b631bccca1130bf201662820f8970d2cf
SnowleopardXI <69493681+SnowleopardXI@users.noreply.github.com>
2020-08-11 15:59:19 +0800

    Create Shortlink.py

---
+++ bin/Shortlink.py
@@ -0,0 +1,18 @@
+import requests
+import argparse
+import sys
+def main(args):
+    print("-id {0}".format(args.id))
+def url():
+    long_url = inp
+    querystring = {"url":long_url}
+    url = "http://suo.im/api.php"
+    response = requests.request("GET", url, params=querystring)
+    print(response.text)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(usage="it's usage tip.", description="help info.")
+    parser.add_argument("-url", type=str, required=True, help="The Origin URL.")
+    args = parser.parse_args()
+    main(args)
+    inp = args.id
+    url()
