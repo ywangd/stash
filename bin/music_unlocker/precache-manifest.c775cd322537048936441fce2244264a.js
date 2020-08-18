46344c998fa37ed37ba7e7a3ee6c378b62d49909
SnowleopardXI <69493681+SnowleopardXI@users.noreply.github.com>
2020-08-18 22:16:57 +0800

    Added music unlocker

---
+++ bin/music_unlocker/precache-manifest.c775cd322537048936441fce2244264a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df1b09da573ce5338f7962070effa0d",
+    "url": "0990e3de9f7bfba56605.worker.js"
+  },
+  {
+    "revision": "2beea626388f5a29104d",
+    "url": "css/app.2a97a82f.css"
+  },
+  {
+    "revision": "271b59bb71bb016bc588",
+    "url": "css/chunk-vendors.8cf7dd44.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "7c07864f6869cf3d421f6fbb04e5693a",
+    "url": "index.html"
+  },
+  {
+    "revision": "2beea626388f5a29104d",
+    "url": "js/app.13933d42.js"
+  },
+  {
+    "revision": "271b59bb71bb016bc588",
+    "url": "js/chunk-vendors.c65d1814.js"
+  },
+  {
+    "revision": "28f3e13ec88073aa1b85b7d66358f613",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "523b1a2eae8cb533fa6bd73831308f09",
+    "url": "static/kgm.mask"
+  }
+]);
