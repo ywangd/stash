f0847ef02528dc53afcecef384da709f7fc9ee9b
Frank Schäckermann <github-fschaeckermann@snkmail.com>
2021-04-19 09:51:25 +0200

    removed requirement of pathlib2 and globals

--- bin/grep.py
+++ bin/grep.py
@@ -8,57 +8,142 @@ import io
 import re
 import sys
 import argparse
-import fileinput
 import fnmatch
-from six import PY3
-
-if PY3:
-    from pathlib import Path
-else:
-    from pathlib2 import Path
 
 _stash = globals()['_stash']
-abbreviate = _stash.libcore.abbreviate
 
-new_file = True
-count = 0        
-ns = None
-fn = None
 
-def open_hook(filename,mode):
-    global fn
-    global new_file
-    global count
-    global ns
-    global abbreviate
+class Path(object):
+    def __init__(self,pathname):
+        self.pathname = pathname
+        self.real_pathname = os.path.realpath(pathname)
+        
+    def __str__(self):
+        return self.pathname
     
-    if fn:
-        print_on_count(fn)
-    new_file = True
-    count = 0
-    if filename:
-        fn = abbreviate(filename)
-        return io.open(filename,mode=mode,encoding='latin1')
-    else:
-        fn = ns.label
-        return None
+    def pretty(self):
+        return _stash.libcore.abbreviate(self.pathname)
+    
+    def is_file(self):
+        return os.path.isfile(self.real_pathname)
+    
+    def is_dir(self):
+        return os.path.isdir(self.real_pathname)
+    
+    @property
+    def parent(self):
+        return Path(os.path.dirname(self.pathname))
+    
+    @property 
+    def name(self):
+        return os.path.basename(self.pathname)
+    
+    def rglob(self,pattern):
+        """ returns an iterable through all files
+            matching the specified pattern in this
+            directory (self.pathname must denote
+            a directory - and the caller is taking
+            care of this in this script therefore
+            no check is necessary here)
+        """
+        for root,_,files in os.walk(self.pathname):
+            for fn in fnmatch.filter(files,pattern):
+                yield Path(os.path.join(root,fn))
 
 
-def print_on_count(filename):
-    global count
-    global ns
-    if (ns.count and (ns.files_without_match or count>0)):
-        print(u'{cnt:6} {fn}'
-              .format(cnt=count,fn=filename))
-    elif ns.files_without_match and count==0:
-        print(filename)
-            
+class CountOutputHandler(object):
+    """ An object of this class keeps track on the count
+        of matches for the file currently being processed.
+        through the method new_file it is informed whenever
+        processing of a new file is started.
+        It also provides a "pretty" name (filename_to_print)
+        to be used in the printed output of the grep command.
+    """
+    def __init__(self,files_without_match=False,
+                      count_only=False):
+        self.files_without_match = files_without_match
+        self.count_only = count_only
+        self.count = 0
+        self.filename_to_print = None
+        
+    def new_file(self,filename_to_print):
+        if self.filename_to_print:
+            self.print_on_count()
+        self.count = 0
+        self.filename_to_print = filename_to_print
+
+    def count_match(self):
+        self.count += 1
+        
+    def print_on_count(self):
+        """ If either the --count or --files-without-match option is
+            active, this method takes care of creating the necessary
+            output after a file finished processing.
+            Otherwise it does nothing.
+        """
+        if (self.count_only and (self.files_without_match or self.count>0)):
+            print(u'{cnt:6} {fn}'
+                  .format(cnt=self.count,fn=self.filename_to_print))
+        elif self.files_without_match and self.count==0:
+            print(self.filename_to_print)
+
+
+class FileInputHandler(object):
+    """ This class provides input lines from a series
+        of files opening and closing them as needed.
+        Also for every new file opened a callback is
+        used to signal the change of the input file.
+    """
+    def __init__(self,filepaths,
+                      new_file_callback,
+                      stdin_label='<stdin>',
+                      encoding='latin1'):
+
+        self.filepaths = filepaths
+        self.new_file_callback = new_file_callback
+        self.stdin_label = stdin_label
+        self.encoding = encoding
+        self.lineno = 0
+        
+        self.keep_reading_file = True
+        
+    def nextfile(self):
+        """ This method is called to signal, that the 
+            rest of the file currently being read is
+            to be skipped.
+        """
+        self.keep_reading_file = False
+                    
+    def input(self):
+        """ This method is an iterator over all the lines of
+            the combined input files passed in, when this
+            object was created.
+            Calls to nextfile() can have the rest of the current
+            file being skipped.
+        """
+        while len(self.filepaths)>0:
+            filepath = self.filepaths.pop(0)
+            if filepath:
+                self.new_file_callback(filepath.pretty())
+                filehndl = io.open(str(filepath),mode='rt',encoding=self.encoding)
+            else:
+                self.new_file_callback(self.stdin_label)
+                filehndl = sys.stdin
+
+            self.keep_reading_file = True
+            self.lineno = 1
+            line = filehndl.readline()
+            while len(line)>0 and self.keep_reading_file:
+                yield line
+                self.lineno += 1
+                line = filehndl.readline()
+ 
+            if filehndl!=sys.stdin:
+                filehndl.close()
+                
+        self.new_file_callback(None) # signal callback we are done
+    
 def main(args):
-    global fn
-    global new_file
-    global count
-    global ns
-    global _stash
     ap = argparse.ArgumentParser(add_help=False)
     ap.add_argument('--help', action='help', help='show this help message and exit')
     ap.add_argument('pattern', help='the pattern to match')
@@ -75,76 +160,76 @@ def main(args):
     ap.add_argument('-l','--files-with-matches', action='store_true', help='only list file names of files that match')
     ap.add_argument('-o','--only-matching', action='store_true', help='only print the matching parts')
     ap.add_argument('--color', action='store_true', help='color matched text in output')
-    ns = ap.parse_args(args)
+    ap.add_argument('--encoding', action='store', default='latin1',help=='encoding to use when opening files (default encoding is latin1)')
 
-    flags = 0
-    if ns.ignore_case:
-        flags |= re.IGNORECASE
-
-    pattern = re.compile(ns.pattern, flags=flags)
-
-    files = []
-    if ns.recursive:
-        for f in ns.files:
-            if f=="-":
-                files.append(f)
-            else:
-                p = Path(f)
-                if p.is_dir():
-                    files += [ str(ff.resolve()) for ff in p.rglob('*') if ff.is_file()]
-                else:
-                    d = p.parent
-                    n = p.name
-                    files += [ str(ff.resolve()) for ff in d.rglob(n) if ff.is_file()]
-    elif len(ns.files)>0:
-        # Do not try to grep directories
-        for f in ns.files:
-            if f=="-":
-                files.append(f)
-            else:
-                p = Path(f)
-                if p.is_file():
-                    files.append(str(p.resolve()))
-                elif not p.exists():
-                    _stash.write_message('{} was skipped because it does not exist'.format(f),prefix="grep: ",error=True)
-                else:
-                    _stash.write_message('{} was skipped because it is not a file'.format(f),prefix="grep: ",error=True)
-        if len(files)==0:
-            _stash.write_message('No valid files given. Aborting.',prefix="grep: ",error=True)
-            return
-                
-
-    no_filename = (len(files)<=1)
-    if ns.no_filename:
-        no_filename = True
-    if ns.with_filename:
-        no_filename = False
-        
-    if no_filename:
-        if ns.line_number:
-            fmt = u'{lineno}: {line}'
-        else:
-            fmt = u'{line}'
-    elif ns.line_number:
-        fmt = u'{filename}: {lineno}: {line}'
-    else:
-        fmt = u'{filename}: {line}'
-    
-    fileinput.close()  # in case it is not closed
+    rc = 0
     try:
-        count = 0
-        lastfile = None
-        for line in fileinput.input(files, openhook=open_hook):
-            if fileinput.isfirstline() and fileinput.isstdin():
-                # simulate open for stdin
-                open_hook(None,None)
-                
+        ns = ap.parse_args(args)
+    
+        flags = 0
+        if ns.ignore_case:
+            flags |= re.IGNORECASE
+    
+        pattern = re.compile(ns.pattern, flags=flags)
+    
+        filepaths = []
+        if ns.recursive:
+            for f in ns.files:
+                if f=="-":
+                    filepaths.append(None)
+                else:
+                    p = Path(f)
+                    if p.is_dir():
+                        filepaths += p.rglob('*')
+                    else:
+                        d = p.parent
+                        n = p.name
+                        filepaths += d.rglob(n)
+        elif len(ns.files)==0:
+            filepaths.append(None)
+        else:
+            # Do not try to grep directories
+            for f in ns.files:
+                if f=="-":
+                    filepaths.append(None)
+                else:
+                    p = Path(f)
+                    if p.is_file():
+                        filepaths.append(p)
+                    elif not p.exists():
+                        print('grep: {} was skipped because it does not exist'.format(f),file=sys.stderr)
+                    else:
+                        print('grep: {} was skipped because it is not a file'.format(f),file=sys.stderr)
+            if len(filepaths)==0:
+                print('grep: No valid files given. Exiting.',file=sys.stderr)
+                return 1
+                    
+    
+        no_filename = (((len(filepaths)<=1)
+                         or ns.no_filename)
+                       and not ns.with_filename)
+            
+        if no_filename:
+            if ns.line_number:
+                fmt = u'{lineno}: {line}'
+            else:
+                fmt = u'{line}'
+        elif ns.line_number:
+            fmt = u'{filename}: {lineno}: {line}'
+        else:
+            fmt = u'{filename}: {line}'
+    
+        count_output_handler = CountOutputHandler(files_without_match=ns.files_without_match,
+                                                  count_only=ns.count)
+        file_input_handler = FileInputHandler(filepaths, count_output_handler.new_file, ns.label, ns.encoding)
+
+        for line in file_input_handler.input():
             if bool(pattern.search(line))!=ns.invert:
-                count += 1
+                count_output_handler.count_match()
                 if (ns.files_with_matches
                     and not ns.invert):
-                    print(fn)
-                    fileinput.nextfile()
+                    print(count_output_handler.filename_to_print)
+                    file_input_handler.nextfile()
                 elif not (ns.count or
                           ns.files_without_match):
                     if ns.invert:
@@ -161,14 +246,16 @@ def main(args):
                         lines = [ line ]
                     
                     for ln in lines:
-                        print(fmt.format(filename=fn, lineno=fileinput.filelineno(), line=ln),end='')
-
-        print_on_count(fn)
+                        print(fmt.format(filename=count_output_handler.filename_to_print,
+                                         lineno=file_input_handler.lineno,
+                                         line=ln),
+                              end='')
 
     except Exception as err:
-        print("grep: {}: {!s}".format(type(err).__name__, err), file=sys.stderr)
-    finally:
-        fileinput.close()
+        print("grep: {}: {s}".format(type(err).__name__, err), file=sys.stderr)
+        rc = 1
+    
+    return rc
 
 
 if __name__ == "__main__":
