9aff137bca0afc36be67ee7a16cb25e28348a569
Frank Schäckermann <github-fschaeckermann@snkmail.com>
2022-06-21 20:40:03 +0200

    Attempt to fix tests

--- tests/pip/test_pip.py
+++ tests/pip/test_pip.py
@@ -136,7 +136,7 @@ class PipTests(StashTestCase):
     @requires_network
     def test_install_pypi_onlybinary(self):
         """test 'pip install --only-binary :all: <pypi_package>'."""
-        output = self.run_command("pip --verbose install --only-binary :all: rsa", exitcode=0)
+        output = self.run_command("pip --verbose install --only-binary :all: rsa==4.5", exitcode=0)
         self.assertIn("Downloading package", output)
         self.assert_did_run_setup(output, allow_source=False)
         self.assertIn("Package installed: rsa", output)
@@ -153,7 +153,7 @@ class PipTests(StashTestCase):
         self.run_command("pyrsa-keygen --help", exitcode=127)
 
         # 2. install
-        output = self.run_command("pip --verbose install rsa", exitcode=0)
+        output = self.run_command("pip --verbose install rsa==4.5", exitcode=0)
         self.assertIn("Downloading package", output)
         self.assert_did_run_setup(output)
         self.assertIn("Package installed: rsa", output)
