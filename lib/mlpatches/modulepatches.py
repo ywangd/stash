dff9ce55afdb1c61077f074f01bf87442985f07f
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2025-08-14 10:34:33 +0300

    get popen patches back

--- lib/mlpatches/modulepatches.py
+++ lib/mlpatches/modulepatches.py
@@ -3,7 +3,29 @@
 
 from mlpatches import base
 
-assert base
+
+class Popen2Patch(base.ModulePatch):
+    """the patch for the popen2 module."""
+
+    PY3 = False
+    relpath = "popen2.py"
+    name = "popen2"
+
+
+class SubprocessPatch(base.ModulePatch):
+    """the patch for the subprocess module."""
+
+    PY3 = False  # uses unicode
+    relpath = "subprocess.py"
+    name = "subprocess"
+
+
+# create instances
+POPEN2PATCH = Popen2Patch()
+SUBPROCESSPATCH = SubprocessPatch()
 
 # name -> ModulePatch()
-MODULE_PATCHES = {}
+MODULE_PATCHES = {
+    "popen2": POPEN2PATCH,
+    "subprocess": SUBPROCESSPATCH,
+}
